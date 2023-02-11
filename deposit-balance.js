a21540384af74b5bd409820e4f8d0d77e41a6e42
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-12 02:42:10 +0600

    totalbalance added

---
+++ deposit-balance.js
@@ -0,0 +1,38 @@
+//deposit with balance connection ------------------>
+
+// deposit ------------------------>
+document.getElementById('deposit-btn').addEventListener('click',function(){
+
+    // step-1 : input deposit amount text to float:
+
+    const inputTextAmount = document.getElementById('deposit-input');
+
+    const inputAmount = parseFloat(inputTextAmount.value);
+
+    // step-2 : balance deposit text to float :
+
+    const insideTextAmount = document.getElementById('dolar');
+
+    const insideAmount = parseFloat(insideTextAmount.innerText);
+
+    // step-3 : Total
+
+    const totalAmount = inputAmount + insideAmount;
+
+    insideTextAmount.innerText = totalAmount;
+
+// connect deposit with balance 
+
+   const textBalance =document.getElementById('balance');
+
+   const floatBalance = parseFloat(textBalance.innerText);
+
+   const totalBalance = floatBalance * totalAmount;
+
+   textBalance.innerText = totalBalance;
+
+// make input filed empty
+
+   inputTextAmount.value = "";
+
+});
