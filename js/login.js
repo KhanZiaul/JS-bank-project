add9c3d814c8a2541336809ef4dcc448bdc7ece7
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-12 22:49:04 +0600

    login.js bug fixed

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ document.getElementById('sub-btn').addEventListener('click',function(){
 
 
     if(email.value === 'khanzia@gmail.com' && password.value === 'khanzia123'){
-        location.href = 'http://127.0.0.1:5500/bank.html';
+        location.href = '/bank.html';
     }
 
     else{
